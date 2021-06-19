2e7a685e1143bad6bd3e18734f230cd3f1e45f8a
Benoît Hubert <benoithubert@gmail.com>
2021-06-19 02:11:40 +0200

    Add support for client-side auth code flow w/ PKCE

--- example/client/src/App.js
+++ example/client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ImplicitGrantExample from './ImplicitGrantExample';
+import PKCEExample from './PKCEExample';
 import AuthorizationCodeExample from './AuthorizationCodeExample';
 
 const onFailure = response => console.error(response);
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <div className="App">
       <ImplicitGrantExample />
+      <PKCEExample />
       <AuthorizationCodeExample />
     </div>
   );
